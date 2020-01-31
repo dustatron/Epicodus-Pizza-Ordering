38806fa7a30cf9d69d9f097fac202a907dc1a8e9
Dusty <dustymccord@gmail.com>
2020-01-31 10:42:29 -0800

    add a find method to pizzeria and a toDeliver method to Card.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,19 @@
 //////// Pizzeria Object ////////
 function Pizzeria() {
 	this.orders = [];
-	this.toppings = {};
-	this.pSizes = {};
+	this.toppings = {
+		cheese: 'img/cheese.jpeg',
+		pepperoni: 'img/cheese.jpeg',
+		artichoke: 'img/artichoke',
+		anchovy: 'img/anchovy',
+		bacon: 'img/bacon'
+	};
+	this.pSizes = {
+		large: 18,
+		medium: 14,
+		small: 12,
+		xsmall: 2
+	};
 	this.cartId = 0;
 	this.pizzaId = 0;
 }
@@ -18,9 +29,16 @@ Pizzeria.prototype.addCart = function(cart) {
 	this.orders.push(cart);
 };
 
+Pizzeria.prototype.find = function(id) {
+	for (let i = 0; i < this.orders.length; i++) {
+		if (this.orders[i] === id) {
+			return this.orders[i];
+		}
+	}
+};
+
 //////////////////////////////////
 ////////// Cart Object //////////
-
 function Cart() {
 	this.items = [];
 	this.toDeliver = false;
@@ -41,14 +59,12 @@ Cart.prototype.addPizza = function(size, toppings) {
 	this.items.push(pizza);
 };
 
+Cart.prototype.toDeliver = function(option) {
+	this.toDeliver = option;
+};
+
 var pizzeria = new Pizzeria();
 
 $(document).ready(function() {
 	console.log('JavaScipt is working');
-
-	//on click function
-	$('form').submit(function(event) {
-		event.preventDefault();
-		console.log('click');
-	});
 });
